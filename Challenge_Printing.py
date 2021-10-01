9699d09ccfa5f3b740a5f28023c2cc185e9beb7b
Manish Kumar Goswami <53654018+ghostmanish@users.noreply.github.com>
2021-10-02 04:32:41 +0530

    Challenge

---
+++ Challenge_Printing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 00:44:49 2021
+
+@author: Mnish Kumar Goswami
+"""
+
+print("Day 1 - Python Print Function")
+print("The function is declred like this:")
+print("print('What to print')")
+print("Hello World!\nHello World!")
+print("Hello"+" Manish")
+print("Hello "+"Manish")
+print("Hello"+" " + "Manish")
